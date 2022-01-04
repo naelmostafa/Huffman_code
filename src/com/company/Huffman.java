930b99d5f46ae6fb67df8a65c21428a44f209c51
Nael Mansour <naelnael53.nm@gmail.com>
2022-01-04 16:36:07 +0200

    Compress and Decompress option -- refactor

--- src/com/company/Huffman.java
+++ src/com/company/Huffman.java
@@ -8,16 +8,18 @@ public class Huffman {
     private Map<Character, Integer> freqMap;
     private Map<Character, String> codeword;
 
-    public Huffman(String text) throws IOException {
-        readGenerateFreq(text);
-        creatTree();
-        generateCodeword();
+    public Huffman(char type, String filename) throws IOException {
+        if (type == 'c') {
+            encode(filename);
+        } else if (type == 'd') {
+            decode(filename);
+        }
     }
 
-    private void readGenerateFreq(String fileName) throws IOException {
+    private void readGenerateFreq(String filename) throws IOException {
         freqMap = new HashMap<>();
         /* Creation of File Descriptor for input file */
-        File f = new File(fileName);
+        File f = new File(filename);
 
         /* Creation of File Reader object */
         FileReader fr = new FileReader(f);
@@ -28,6 +30,9 @@ public class Huffman {
         /* Read char by Char */
         int c = 0;
         while ((c = br.read()) != -1) {
+            if (c == ' '){
+                continue;
+            }
             char character = (char) c;          // converting integer to char
             freqMap.put(character, freqMap.getOrDefault(character, 0) + 1);
         }
@@ -60,7 +65,24 @@ public class Huffman {
         __generateCodeword(node.getRightNode(), code.concat("1"));
     }
 
-    /* TODO: Encode replace every character with its corresponding code word */
-    private void encode() {
+    /*  output extension ==> <id>.<n>.abc.exe.hc */
+    public void encode(String filename) throws IOException {
+        readGenerateFreq(filename);
+        creatTree();
+        generateCodeword();
+        /* TODO: Encode replace every character with its corresponding code word */
+
+    }
+
+    /* TODO: read after -1 => codewords ex: a:0 b:10 delimiter = ":" --- generate hashmap<Codeword>*/
+    public void decode(String filename) {
+        readEncoded(filename);
+        translateFile(filename, codeword);
+    }
+
+    private void translateFile(String filename, Map<Character, String> codeword) {
+    }
+
+    private void readEncoded(String filename) {
     }
 }
